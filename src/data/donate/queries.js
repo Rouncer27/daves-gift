f79bac213b952c90c1b5fa5c0a6e032294215f90
Trevor Rounce <trevor@switchbackcreative.ca>
2026-06-09 10:23:33 -0600

    added new page templates

---
+++ src/data/donate/queries.js
@@ -0,0 +1,16 @@
+export const Hero_Query = `
+     hero: heroComponent {
+        title
+        content
+        image {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+    }
+`;
