ac3056cccb39c37af762e77d53c94118002cd50f
Trevor Rounce <trevor@switchbackcreative.ca>
2026-06-10 09:45:07 -0600

    work on the get involved page

--- src/data/getInvolved/queries.js
+++ src/data/getInvolved/queries.js
@@ -14,3 +14,81 @@ export const Hero_Query = `
         }
     }
 `;
+
+export const Upcoming_Events_Query = `
+     upcomingEvents: upcomingEventsComponent {
+        title
+         event {
+            edges {
+                node {
+                    ... on Event {
+                        title
+                        slug
+                        eventAcf {
+                            excerpt
+                            featuredImage {
+                                node {
+                                    sourceUrl
+                                    altText
+                                    mediaDetails {
+                                        width
+                                        height
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
+
+export const Four_Blocks_Query = `
+     fourBlocks: fourBlocksComponent {
+        titleTop
+        contentTop
+        buttonTopText
+        imageTop {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+        titleBottom
+        contentBottom
+        buttonBottomText
+        imageBottom {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+
+        buttonTopLink {
+            edges {
+                node {
+                    slug
+                    uri
+                }
+            }
+        }
+        
+        buttonBottomLink {
+            edges {
+                node {
+                    slug
+                    uri
+                }
+            }
+        }
+    }
+`;
