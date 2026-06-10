16c3f653723cb71549ec0393f0ccd2ffc1877d24
Trevor Rounce <trevor@switchbackcreative.ca>
2026-06-10 13:24:48 -0600

    work on the get involved page

--- src/data/advocacy/queries.js
+++ src/data/advocacy/queries.js
@@ -14,3 +14,78 @@ export const Hero_Query = `
         }
     }
 `;
+
+export const Advocacy_Intro_Query = `
+     advocacyIntro: advocacyIntroComponent {
+        title
+        content
+    }
+`;
+
+export const Get_Involved_Query = `
+     getInvolved: getInvolvedComponent {
+        title
+        content
+        buttonOneText
+        buttonTwoText
+        image {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+        buttonOneLink {
+            edges {
+                node {
+                    uri
+                }
+            }
+        }
+        buttonTwoLink {
+            edges {
+                node {
+                    uri
+                }
+            }
+        }
+    }
+`;
+
+export const Image_Row_Query = `
+     imageRow: imageRowComponent {
+        imageOne {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+        imageTwo {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+        imageThree {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+    }
+`;
